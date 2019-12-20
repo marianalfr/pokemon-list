12fbd5587232c7effa45351e876c383e07a96f10
marianalfr <marianalfr@gmail.com>
2019-12-20 16:50:18 +0100

    create docs

---
+++ docs/precache-manifest.d76cce692d3ddb185f6a69a3a7550ff9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "937e5f76988c3a9355f6ed7d40d69d7a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9354da3a5a3ed459e3b3",
+    "url": "./static/css/main.edd71313.chunk.css"
+  },
+  {
+    "revision": "8d60dcbc57f3b32b58d9",
+    "url": "./static/js/2.4ba2f448.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.4ba2f448.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9354da3a5a3ed459e3b3",
+    "url": "./static/js/main.28fd3dc7.chunk.js"
+  },
+  {
+    "revision": "60b18f345a8d9df7056c",
+    "url": "./static/js/runtime-main.fc282c3a.js"
+  }
+]);
